8a8bb09e6c0a2f3c66c8e727cdb6abbf0f78a030
Will Moller <will.moller@gmail.com>
2021-03-01 15:30:50 -0600

    Started Drill GUI and comments for logic on button click events.

---
+++ MathWizzz/MathWizzz/frmDrill.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MathWizzz
+{
+    public partial class frmDrill : Form
+    {
+        public frmDrill()
+        {
+            InitializeComponent();
+        }
+        private void frmDrill_Load(object sender, EventArgs e)
+        {
+            // generate first Question object.
+        }
+
+        private void btnSubmitAnswer_Click(object sender, EventArgs e)
+        {
+            // Validate answer is in proper format
+
+            // Compare user's answer with answer property of Question object
+
+            // Display if answer is correct or not to user (Modal pop-up?)
+
+            // If answer was correct or user is out of attempts, load new question.
+            // Otherwise, clear Answer field and put cursor there.
+
+        }
+
+        private void btnCancelDrill_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        
+    }
+}
