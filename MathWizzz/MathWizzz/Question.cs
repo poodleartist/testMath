70a5deeb959a5fee62550b8d2ad9422d57205205
Will Moller <will.moller@gmail.com>
2021-03-01 15:53:23 -0600

    Created Question class.

---
+++ MathWizzz/MathWizzz/Question.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace MathWizzz
+{
+    public class Question
+    {
+        public int SkillLevel { get; set; }
+        public string NewQuestion { get; set; }
+        public int Answer { get; set; }
+
+        public Question(int skillLevel)
+        {
+            SkillLevel = skillLevel;
+            GenerateQuestionAndAnswer();
+        }
+
+        private void GenerateQuestionAndAnswer()
+        {
+            Random random = new Random();
+            int firstOperand = random.Next()
+        }
+    }
+}
