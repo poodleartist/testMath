d31932e7ca15596cca73d767fbe5508fcfc4b53d
Tai Mai <tm561400@southeast.edu>
2021-03-01 16:09:47 -0600

    Added a homepage. Also, load number of question to the form. User can make a selection of how many question they want to take.

---
+++ MathWizzz/MathWizzz/frmHomePage.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace MathWizzz
+{
+    partial class frmHomePage
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.btnProfile = new System.Windows.Forms.Button();
+            this.btnReward = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.cbxDrill = new System.Windows.Forms.ComboBox();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(40, 40);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(892, 49);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(75, 45);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(76, 45);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // btnProfile
+            // 
+            this.btnProfile.Location = new System.Drawing.Point(251, 125);
+            this.btnProfile.Name = "btnProfile";
+            this.btnProfile.Size = new System.Drawing.Size(380, 66);
+            this.btnProfile.TabIndex = 1;
+            this.btnProfile.Text = "Profile";
+            this.btnProfile.UseVisualStyleBackColor = true;
+            // 
+            // btnReward
+            // 
+            this.btnReward.Location = new System.Drawing.Point(251, 312);
+            this.btnReward.Name = "btnReward";
+            this.btnReward.Size = new System.Drawing.Size(380, 66);
+            this.btnReward.TabIndex = 3;
+            this.btnReward.Text = "Reward Points";
+            this.btnReward.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(251, 410);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(380, 66);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // cbxDrill
+            // 
+            this.cbxDrill.FormattingEnabled = true;
+            this.cbxDrill.Location = new System.Drawing.Point(251, 217);
+            this.cbxDrill.Name = "cbxDrill";
+            this.cbxDrill.Size = new System.Drawing.Size(380, 39);
+            this.cbxDrill.TabIndex = 5;
+            this.cbxDrill.Text = "Drills";
+            this.cbxDrill.SelectedIndexChanged += new System.EventHandler(this.cbxDrill_SelectedIndexChanged);
+            // 
+            // frmHomePage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(892, 582);
+            this.Controls.Add(this.cbxDrill);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.btnReward);
+            this.Controls.Add(this.btnProfile);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "frmHomePage";
+            this.Text = "frmHomePage";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.Button btnProfile;
+        private System.Windows.Forms.Button btnReward;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.ComboBox cbxDrill;
+    }
+}
