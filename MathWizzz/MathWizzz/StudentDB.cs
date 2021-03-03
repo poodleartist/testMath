90849328d509a7db24e7ec81e5fbea7871a3d07a
Lydia Williams <lw297113@southeast.edu>
2021-03-03 12:11:23 -0600

    Added StudentDB class for student related accessing the DB. Edited ViewProgress Form to test if code for connecting to DB works. Unfortunately wasn't able to test it yet.

---
+++ MathWizzz/MathWizzz/StudentDB.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathWizzz
+{
+    class StudentDB
+    {
+        public static Student GetStudentById(int studentId)
+        {
+            SqlConnection connection = MathWizzDB.GetConnection();
+            string selectStatement = "SELECT StudentId, StudentLevel," +
+                "ClassId,FirstName FROM StudentInfo" +
+                "JOIN Users on StudentId = UserId" +
+                "WHERE StudentId = @StudentId";
+            SqlCommand selectCommand = new SqlCommand(selectStatement, connection);
+            selectCommand.Parameters.AddWithValue("@StudentId", studentId);
+            try
+            {
+                connection.Open();
+                SqlDataReader studentReader = selectCommand.ExecuteReader(System.Data.CommandBehavior.SingleRow);
+                if(studentReader.Read())
+                {
+                    Student student = new Student();
+                    student.StudentID = (int)studentReader["StudentId"];
+                    student.FirstName = studentReader["FirstName"].ToString();
+                    student.StudentLevel = (int)studentReader["StudentLevel"];
+                    student.ClassID = (int)studentReader["ClassId"];
+                    return student;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+            catch (SqlException ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+
+
+
+    }
+}
