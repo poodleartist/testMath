b6b1ea8338471e6ca26bee47efd1c894c529303d
Will Moller <will.moller@gmail.com>
2021-03-03 16:15:29 -0600

    Updated frmDrill, Drill, and Question classes to clean up some logic.

--- MathWizzz/MathWizzz/frmDrill.cs
+++ MathWizzz/MathWizzz/frmDrill.cs
@@ -15,31 +15,55 @@ namespace MathWizzz
         public Question Question { get; set; }
         public SkillLevel SkillLevel { get; set; }
         public int UserID { get; set; }
+        public Drill Drill { get; set; }
 
-        public frmDrill()
+        public frmDrill(SkillLevel skillLevel)
         {
             InitializeComponent();
+            SkillLevel = skillLevel;
         }
         private void frmDrill_Load(object sender, EventArgs e)
         {
             // generate SkillLevel object
-            SkillLevel skillLevel = new SkillLevel();
+            SkillLevel = new SkillLevel();
             
 
             // generate first Question object.
-            Question = new Question(
-                student.skillLevelID,
-                skillLevel.NumberOfDigits,
-                skillLevel.NumberOfDecimalPlaces,
-                skillLevel.MinValue,
-                skillLevel.MaxValue,
-                skillLevel.MathOperator);
+            Question = new Question(SkillLevel);
+            DisplayQuestion();
+        }
+
+        private void DisplayQuestion()
+        {
+            txtQuestion.Text = Question.NewQuestion;
         }
 
         private void btnSubmitAnswer_Click(object sender, EventArgs e)
         {
             // Validate answer is in proper format
-
+            try
+            {
+                
+                int answer = int.Parse(txtAnswer.Text);
+                bool isCorrect = Drill.CheckAnswer(answer);
+                if (!isCorrect && Drill.NumberOfAttempts > 0)
+                {
+                    MessageBox.Show("Incorrect answer.\n\nTry again.", "Incorrect answer.");
+                } else if (!isCorrect)
+                {
+                    MessageBox.Show($"Incorrect answer.\n\nThe correct answer is: {Question.Answer}", "Incorrect Answer");
+                } else
+                {
+                    MessageBox.Show("Correct!", "Correct Answer");
+                }
+                
+            } catch (FormatException)
+            {
+                MessageBox.Show("Please enter a number.", e.GetType().ToString());
+            } catch (Exception f)
+            {
+                MessageBox.Show(f.Message, f.GetType().ToString());
+            }
             // Compare user's answer with answer property of Question object
 
             // Display if answer is correct or not to user (Modal pop-up?)
