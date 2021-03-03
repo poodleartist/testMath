e73fa10fc4307e33fb2f9b1f6a3b7bda58b8d648
Tyler Johnson <johnsonptyler@yahoo.com>
2021-03-03 15:23:19 -0600

    .

--- MathWizzz/MathWizzz/DataAccess.cs
+++ MathWizzz/MathWizzz/DataAccess.cs
@@ -13,37 +13,38 @@ namespace MathWizzz
         {
             bool success = false;
             SqlConnection connectionString = MathWizzDB.GetConnection();
-            using (SqlConnection connection = new SqlConnection(connectionString))
-            {
-                SqlCommand command = new SqlCommand("INSERT INTO Users (UserName, Password, FirstName, LastName, UserRole)" +
+            SqlCommand command = new SqlCommand("INSERT INTO Users (UserName, Password, FirstName, LastName, UserRole)" +
                                                 "VALUES(@UserName, @Password, @FirstName, @LastName, @UserRole)");
 
-                command.Parameters.AddWithValue("@UserName", person.Username);
-                command.Parameters.AddWithValue("@Password", person.Password);
-                command.Parameters.AddWithValue("@FirstName", person.FirstName);
-                command.Parameters.AddWithValue("@LastName", person.LastName);
-                command.Parameters.AddWithValue("@UserRole", person.UserRole);
+            command.Parameters.AddWithValue("@UserName", person.Username);
+            command.Parameters.AddWithValue("@Password", person.Password);
+            command.Parameters.AddWithValue("@FirstName", person.FirstName);
+            command.Parameters.AddWithValue("@LastName", person.LastName);
+            command.Parameters.AddWithValue("@UserRole", person.UserRole);
 
-                try
+            try
+            {
+                connectionString.Open();
+                int rowsAffected = command.ExecuteNonQuery();
+                if (rowsAffected > 0)
                 {
-                    connection.Open();
-                    int rowsAffected = command.ExecuteNonQuery();
-                    if (rowsAffected > 0)
-                    {
-                        success = true;
-                    } else
-                    {
-                        success = false;
-                    }
-
-                    // for testing
-                    Console.WriteLine("RowsAffected: {0}", rowsAffected);
-                    
+                    success = true;
                 }
-                catch
+                else
                 {
-
+                    success = false;
                 }
+
+                // for testing
+                Console.WriteLine("RowsAffected: {0}", rowsAffected);
+
+            }
+            catch(SqlException ex)
+            {
+                throw ex;
+            } finally
+            {
+                connectionString.Close();
             }
 
             return success;
