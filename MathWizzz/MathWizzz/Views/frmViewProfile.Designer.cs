b233be57b5238ab41e88befef79f81bf869a6af9
Tai Mai <tm561400@southeast.edu>
2021-03-10 16:16:06 -0600

    .

--- MathWizzz/MathWizzz/Views/frmViewProfile.Designer.cs
+++ MathWizzz/MathWizzz/Views/frmViewProfile.Designer.cs
@@ -29,6 +29,14 @@
         private void InitializeComponent()
         {
             this.grpEditUser = new System.Windows.Forms.GroupBox();
+            this.btnChangePwd = new System.Windows.Forms.Button();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtNewConfirmPwd = new System.Windows.Forms.TextBox();
+            this.txtNewPwd = new System.Windows.Forms.TextBox();
+            this.txtOldPwd = new System.Windows.Forms.TextBox();
             this.txtSkillLevel = new System.Windows.Forms.TextBox();
             this.txtClassID = new System.Windows.Forms.TextBox();
             this.txtLName = new System.Windows.Forms.TextBox();
@@ -38,14 +46,6 @@
             this.label3 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
-            this.txtOldPwd = new System.Windows.Forms.TextBox();
-            this.txtNewPwd = new System.Windows.Forms.TextBox();
-            this.txtNewConfirmPwd = new System.Windows.Forms.TextBox();
-            this.label6 = new System.Windows.Forms.Label();
-            this.label7 = new System.Windows.Forms.Label();
-            this.label8 = new System.Windows.Forms.Label();
-            this.label9 = new System.Windows.Forms.Label();
-            this.btnChangePwd = new System.Windows.Forms.Button();
             this.grpEditUser.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -74,6 +74,74 @@
             this.grpEditUser.TabIndex = 1;
             this.grpEditUser.TabStop = false;
             this.grpEditUser.Text = "User Profile";
+            // 
+            // btnChangePwd
+            // 
+            this.btnChangePwd.Location = new System.Drawing.Point(343, 556);
+            this.btnChangePwd.Name = "btnChangePwd";
+            this.btnChangePwd.Size = new System.Drawing.Size(255, 57);
+            this.btnChangePwd.TabIndex = 16;
+            this.btnChangePwd.Text = "Confirm Change";
+            this.btnChangePwd.UseVisualStyleBackColor = true;
+            this.btnChangePwd.Click += new System.EventHandler(this.btnChangePwd_Click);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(6, 495);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(316, 32);
+            this.label9.TabIndex = 15;
+            this.label9.Text = "Confirm New Password:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(112, 442);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(210, 32);
+            this.label8.TabIndex = 14;
+            this.label8.Text = "New Password:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.1F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(243, 322);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(261, 32);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Change Password";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(123, 386);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(199, 32);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Old Password:";
+            // 
+            // txtNewConfirmPwd
+            // 
+            this.txtNewConfirmPwd.Location = new System.Drawing.Point(343, 489);
+            this.txtNewConfirmPwd.Name = "txtNewConfirmPwd";
+            this.txtNewConfirmPwd.Size = new System.Drawing.Size(255, 38);
+            this.txtNewConfirmPwd.TabIndex = 11;
+            // 
+            // txtNewPwd
+            // 
+            this.txtNewPwd.Location = new System.Drawing.Point(343, 436);
+            this.txtNewPwd.Name = "txtNewPwd";
+            this.txtNewPwd.Size = new System.Drawing.Size(255, 38);
+            this.txtNewPwd.TabIndex = 10;
+            // 
+            // txtOldPwd
+            // 
+            this.txtOldPwd.Location = new System.Drawing.Point(343, 380);
+            this.txtOldPwd.Name = "txtOldPwd";
+            this.txtOldPwd.Size = new System.Drawing.Size(255, 38);
+            this.txtOldPwd.TabIndex = 9;
             // 
             // txtSkillLevel
             // 
@@ -151,79 +219,11 @@
             this.label1.TabIndex = 0;
             this.label1.Text = "First Name:";
             // 
-            // txtOldPwd
-            // 
-            this.txtOldPwd.Location = new System.Drawing.Point(343, 380);
-            this.txtOldPwd.Name = "txtOldPwd";
-            this.txtOldPwd.Size = new System.Drawing.Size(255, 38);
-            this.txtOldPwd.TabIndex = 9;
-            // 
-            // txtNewPwd
-            // 
-            this.txtNewPwd.Location = new System.Drawing.Point(343, 436);
-            this.txtNewPwd.Name = "txtNewPwd";
-            this.txtNewPwd.Size = new System.Drawing.Size(255, 38);
-            this.txtNewPwd.TabIndex = 10;
-            // 
-            // txtNewConfirmPwd
-            // 
-            this.txtNewConfirmPwd.Location = new System.Drawing.Point(343, 489);
-            this.txtNewConfirmPwd.Name = "txtNewConfirmPwd";
-            this.txtNewConfirmPwd.Size = new System.Drawing.Size(255, 38);
-            this.txtNewConfirmPwd.TabIndex = 11;
-            // 
-            // label6
-            // 
-            this.label6.AutoSize = true;
-            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.1F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(243, 322);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(261, 32);
-            this.label6.TabIndex = 12;
-            this.label6.Text = "Change Password";
-            // 
-            // label7
-            // 
-            this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(123, 386);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(199, 32);
-            this.label7.TabIndex = 13;
-            this.label7.Text = "Old Password:";
-            // 
-            // label8
-            // 
-            this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(112, 442);
-            this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(210, 32);
-            this.label8.TabIndex = 14;
-            this.label8.Text = "New Password:";
-            // 
-            // label9
-            // 
-            this.label9.AutoSize = true;
-            this.label9.Location = new System.Drawing.Point(6, 495);
-            this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(316, 32);
-            this.label9.TabIndex = 15;
-            this.label9.Text = "Confirm New Password:";
-            // 
-            // btnChangePwd
-            // 
-            this.btnChangePwd.Location = new System.Drawing.Point(343, 556);
-            this.btnChangePwd.Name = "btnChangePwd";
-            this.btnChangePwd.Size = new System.Drawing.Size(255, 57);
-            this.btnChangePwd.TabIndex = 16;
-            this.btnChangePwd.Text = "Confirm Change";
-            this.btnChangePwd.UseVisualStyleBackColor = true;
-            this.btnChangePwd.Click += new System.EventHandler(this.btnChangePwd_Click);
-            // 
             // frmViewProfile
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1303, 822);
+            this.ClientSize = new System.Drawing.Size(1850, 822);
             this.Controls.Add(this.grpEditUser);
             this.Name = "frmViewProfile";
             this.Text = "Profile";
