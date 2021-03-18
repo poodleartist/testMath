9ea02aa416b00c74a03ce7960148b891d6813d49
Will Moller <will.moller@gmail.com>
2021-03-17 19:56:28 -0500

    Created Test class.

--- MathWizzz/MathWizzz/Models/Test.cs
+++ MathWizzz/MathWizzz/Models/Test.cs
@@ -3,49 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Windows.Forms;
 
-namespace MathWizzz
+namespace MathWizzz.Models
 {
-    public class Drill : Activity
+    public class Test : Activity
     {
-        public int NumberOfAttempts { get; set; }
-        public int DrillQuestionCount { get; set; }
+        public int TestQuestionCount { get; set; }
         public Question Question { get; set; }
         public Student Student { get; set; }
         public SkillLevel Skill { get; set; }
 
-        public Drill() { }
 
-        public Drill(Student student)
+        public Test (Student student)
         {
             Student = student;
             Skill = SkillLevelDB.GetSkillLevel(Student.StudentLevel);
-            NumberOfAttempts = 1; // student.DrillQuestionAttemps;
-            // NumberOfQuestions = 
-        }
-
-
-        public override Question GetNextQuestion()
-        {
-            if (DrillQuestionCount > 0)
-            {
-                // generate new Question
-                Question = new Question();
-                Question.GenerateQuestionAndAnswer(Skill);
-                NumberOfAttempts = 1; // Student.DrillQuestionAttempts;
-                DrillQuestionCount--;
-                return Question;
-            } else
-            {
-                decimal percentScore = (NumberOfCorrectAnswers / NumberOfQuestions * 100);
-                percentScore = Decimal.Round(percentScore, 0);
-                throw new Exception("Drill is complete.\n\n" +
-                    $"You got {NumberOfCorrectAnswers} out of {NumberOfQuestions} correct for a score of " +
-                    $"{percentScore}%.");
-            }
-            
-            
         }
 
         public override bool CheckAnswer(int studentAnswer)
@@ -55,9 +27,6 @@ namespace MathWizzz
             if (isCorrect)
             {
                 NumberOfCorrectAnswers++;
-            } else
-            {
-                NumberOfAttempts--;
             }
 
             NumberOfQuestions++;
@@ -65,10 +34,30 @@ namespace MathWizzz
             return isCorrect;
         }
 
+        public override Question GetNextQuestion()
+        {
+            if (TestQuestionCount > 0)
+            {
+                // generate new Question
+                Question = new Question();
+                Question.GenerateQuestionAndAnswer(Skill);
+                //NumberOfAttempts = 1; // Student.DrillQuestionAttempts;
+                TestQuestionCount--;
+                return Question;
+            }
+            else
+            {
+                decimal percentScore = (NumberOfCorrectAnswers / NumberOfQuestions * 100);
+                percentScore = Decimal.Round(percentScore, 0);
+                throw new Exception("Test is complete.\n\n" +
+                    $"You got {NumberOfCorrectAnswers} out of {NumberOfQuestions} correct for a score of " +
+                    $"{percentScore}%.");
+            }
+        }
+
         public override bool StoreActivity()
         {
-            // Save completed Drill to the database. Auto-increment Activity ID in database.
-
+            // Save completed Test to the database. Auto-increment Activity ID in database.
 
 
             return true;
